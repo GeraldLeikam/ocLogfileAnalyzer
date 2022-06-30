0ebe90712c9bf25fa53f2d47f9340d731f1a6a22
Gerald <gerald.leikam@aol.com>
2022-06-30 16:30:21 +0200

    Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -5,14 +5,13 @@ import os
 from modules.ui import Ui
 
 if __name__ == '__main__':
-    path = __file__.strip(__file__.split('/')[len(__file__.split('/'))-1])
+    path = __file__.strip(__file__.split('/')[len(__file__.split('/'))-1]) # absolute path
     app = QtWidgets.QApplication(sys.argv)
     try:
         window = Ui(uiTemplate=f'{path}/ui/main.ui')
     except:
-        print(__file__)
-        print(path)
-        window = Ui(uiTemplate=f'{os.getcwd()}/bin/ocLogfileAnalyzer/ui/main.ui') #used for appImage
+        path = f'{path}/ocLogfileAnalyzer' # absolute path for appImage
+        window = Ui(uiTemplate=f'{path}/ui/main.ui')
     sys.exit(app.exec_())
 
 
